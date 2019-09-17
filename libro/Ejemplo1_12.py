e1398f65389b31f7b4a32b534a8b1885da2a7c7c
AngelPerezRodriguezRodriguez <troublemanroade@gmail.com>
2019-09-17 17:14:31 -0500

    Ejercicio en clase 1_12

---
+++ libro/Ejemplo1_12.py
@@ -0,0 +1,4 @@
+a = int(input("Ingresa un valor entero positivo: "))
+b = int(input("Ingresa otro valor entero postivo: "))
+res = (a + b) ** 2 / 3
+print(f"La tercera parte del cuadrado de la suma del primer y segundo dato es: {res}")
