5b5aa088e03347c3bcfef6adf3645b85c95b996d
AngelPerezRodriguezRodriguez <troublemanroade@gmail.com>
2019-09-22 16:16:57 -0500

    Ejercicio en clase 1_8

--- libro/Ejemplo1_8.py
+++ libro/Ejemplo1_8.py
@@ -5,4 +5,4 @@ cal3 = int(input("Ingresa tu calificación de matemáticas: "))
 cal4 = int(input("Ingresa tu calificación de historia: "))
 cal5 = int(input("ingresa tu calificación de geografía: "))
 prom = (cal1 + cal2 + cal3 + cal4 + cal5) / 5
-print(f"Hola, {mat}. Tu promedio final es de {prom}.")
+print(f"Hola, {mat}. Tu promedio final es de {prom}")
