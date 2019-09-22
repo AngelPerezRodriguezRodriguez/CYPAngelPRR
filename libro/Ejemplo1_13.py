a9de3c562742b4a98e3eb3bf0a021b3fc6007836
AngelPerezRodriguezRodriguez <troublemanroade@gmail.com>
2019-09-22 16:18:38 -0500

    Tarea: ejemplos 1_13 a 1_15

---
+++ libro/Ejemplo1_13.py
@@ -0,0 +1,8 @@
+mat = input("Ingresa tu número de matrícula: ")
+cal1 = int(input("Ingresa tu calificación de química: "))
+cal2 = int(input("Ingresa tu calificación de español: "))
+cal3 = int(input("Ingresa tu calificación de matemáticas: "))
+cal4 = int(input("Ingresa tu calificación de historia: "))
+cal5 = int(input("ingresa tu calificación de geografía: "))
+prom = (cal1 + cal2 + cal3 + cal4 + cal5) / 5
+print(f"Hola, {mat}. Tu promedio final es de {prom}")
