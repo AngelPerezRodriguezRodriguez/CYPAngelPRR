49fec496ccdab3d1a9a4ccc0d69ba8e167e7d79d
AngelPerezRodriguezRodriguez <troublemanroade@gmail.com>
2019-10-13 18:00:55 -0500

    Corrección del print de cada carrera

--- libro/problemas_resueltos/2_13.py
+++ libro/problemas_resueltos/2_13.py
@@ -4,10 +4,10 @@ sem = int(input("Ingresa el semestre que estás cursando: "))
 prom = float(input("Ingresa tu promedio actual: "))
 if carr == "Economía":
 	if sem >= 6 and prom >= 8.8:
-		print(f"Felicidades, {mat}. Has sido aceptado en la carrera de {carr}")
+		print(f"Felicidades {mat}, de la carrera de {carr}, has sido aceptado")
 elif carr == "Computación":
 	if sem > 6 and prom > 8.5:
-		print(f"Felicidades, {mat}. Has sido aceptado en la carrera de {carr}")
+		print(f"Felicidades {mat}, de la carrera de {carr}, has sido aceptado")
 elif carr == "Contabilidad" or "Administración":
 	if sem > 5 and prom > 8.5:
-		print(f"Felicidades, {mat}. Has sido aceptado en la carrera de {carr}")
+		print(f"Felicidades {mat}, de la carrera de {carr}, has sido aceptado")
