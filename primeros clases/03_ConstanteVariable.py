f0ba0dfc7dfbb60cd77f2e4199b594beecd45abd
AngelPerezRodriguezRodriguez <troublemanroade@gmail.com>
2019-10-13 22:23:49 -0500

    Primeros días de clases

---
+++ primeros clases/03_ConstanteVariable.py
@@ -0,0 +1,11 @@
+SEMESTRE=1    #constante
+nombre="José" #variable
+
+"""
+esto es un
+comentario
+de multiple
+linea
+"""
+
+print(nombre)
---
+++ primeros clases/04_Operadores.py
@@ -0,0 +1,58 @@
+a = 3
+b = 10 
+c = 0
+
+c = a + b
+print (c)
+
+c = b - a
+print (c)
+
+c = a * b
+print (c)
+
+c = b / a
+print (c)
+
+c = a ** a
+print (c)
+
+c = b // a
+print (c)
+
+c = b % a
+print (c)
+
+c = a + 3 * b - 10
+print (c)
+
+c = (2 + 3)/(5 * 2)
+print (c)
+
+c = 2 + 3 / 5 * 2
+print (c)
+
+c = 2 + 3 * 5 / 2
+print (c)
+
+#operadores relacionales
+
+d = (a == b)
+print (d) 
+
+d = ( a == 3)
+print (d)
+
+d = (a < b)
+print (d)
+
+d = (a != b)
+print (d)
+
+d = (10 != b)
+print (d)
+
+d = b >= 10
+print (d)
+
+#operadores lógicos AND OR NOT
