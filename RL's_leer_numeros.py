6423562499199e3610cc50a8bc5888b398c91ba4
AngelPerezRodriguezRodriguez <troublemanroade@gmail.com>
2019-11-14 13:26:36 -0600

    Correción tarea readlines

--- RL's_leer_numeros.py
+++ RL's_leer_numeros.py
@@ -1,17 +1,18 @@
 archivo = open ("numeros.txt","rt")
 lista = archivo.readlines()
+print(lista)
 
 lista_num = []
+print("")
 for elem in lista:
+    print(elem.strip(),end='')
     for numero in elem.split(','):
         lista_num.append(int(numero))
 
 lista_num.sort()
 
+print("")
 print(f"\nLista ordenada: {lista_num}\n")#Usar más seguido "\n" de esta forma 
 print(f"El número más pequeño de la lista es el {lista_num[1]} y el más grande es el {lista_num[-1]} ")
 
 archivo.close
-
-
-
